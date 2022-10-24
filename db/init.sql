87d9f34cae63cf74565b66bb298681a430385242
nunusmohum <halojw301@gmail.com>
2022-10-24 17:50:02 +0900

    week2 setting_2

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,9 @@
 # 테스트 DB 생성
-DROP DATABASE IF EXISTS final__2022_10_08__test;
-CREATE DATABASE final__2022_10_08__test;
-USE final__2022_10_08__test;
+DROP DATABASE IF EXISTS ebook2__test;
+CREATE DATABASE ebook2__test;
+USE ebook2__test;
 
 # 개발 DB 생성
-DROP DATABASE IF EXISTS final__2022_10_08__dev;
-CREATE DATABASE final__2022_10_08__dev;
-USE final__2022_10_08__dev;
+DROP DATABASE IF EXISTS ebook2__dev;
+CREATE DATABASE ebook2__dev;
+USE ebook2__dev;
